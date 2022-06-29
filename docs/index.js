3e70fbf711aa7b65aeec9e02da6cdaaef3048a7a
IvanovBohdan <kib20-b.ivanov@it.nubip.edu.ua>
2022-06-29 16:24:06 +0300

    kette

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
 const telegram = window.Telegram.WebApp;
 
-const div = document.querySelector(".telegramData");
+const div = document.querySelector("#telegramData");
 div.innerHTML = telegram.initData + "fuckoff";
